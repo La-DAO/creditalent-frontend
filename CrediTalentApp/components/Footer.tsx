ba8fe19727cec8889ff1d6a0b9f4c38ca0b5c892
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2024-12-10 11:13:44 +0100

    Landing and Footer refinements

--- CrediTalentApp/components/Footer.tsx
+++ CrediTalentApp/components/Footer.tsx
@@ -10,18 +10,18 @@ export default function Footer() {
           <div className="mb-8 flex justify-center">
             <h1 className="text-3xl font-bold text-[#FF5722]">CrediTalent</h1>
           </div>
-          <div className="mb-8 flex flex-wrap justify-center gap-6 text-xs text-gray-500">
-            <Link href="#" className="hover:text-gray-700">
-              Terms And Conditions
+          <div className="mb-8 flex flex-wrap justify-center gap-6 text-xs text-[#FF5722]">
+            <Link href="https://sepolia.basescan.org/address/0xBD03d38828Bf0D56f1d325F96d4d48d4a2fa3549#writeProxyContract" className="hover:text-gray-700 text-lg" target="_blank" rel="noopener noreferrer">
+              Live on Base Sepolia
             </Link>
-            <Link href="#" className="hover:text-gray-700">
-              Privacy Policy
+            <Link href="https://github.com/La-DAO/creditalent-contracts" className="hover:text-gray-700 text-lg" target="_blank" rel="noopener noreferrer">
+              CrediTalent Contracts
             </Link>
-            <Link href="#" className="hover:text-gray-700">
-              E-Consent
+            <Link href="https://github.com/La-DAO/creditalent-frontend" className="hover:text-gray-700 text-lg" target="_blank" rel="noopener noreferrer">
+              Frontend Repo
             </Link>
-            <Link href="#" className="hover:text-gray-700">
-              Do Not Sell My Personal Information
+            <Link href="https://devfolio.co/projects/credittalent-42f6" className="hover:text-gray-700 text-lg" target="_blank" rel="noopener noreferrer">
+              DevFolio Pitch Page
             </Link>
           </div>
         </div>
